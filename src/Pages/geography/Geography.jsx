cafe9b91addcfb0db31b9e53c24c9baabae496d5
mohElmorsy555 <mohfaragelmorsy@gmail.com>
2023-11-23 23:23:36 +0200

    third commit

--- src/Pages/geography/Geography.jsx
+++ src/Pages/geography/Geography.jsx
@@ -3,7 +3,7 @@ import { ResponsiveChoropleth } from "@nivo/geo";
 import { Box, useTheme } from "@mui/material";
 import { geo } from "./world_countries";
 import { data } from "./data";
-import Geo from "./geo";
+import Geo from "./Geo";
 import Header from "../../Components/Header";
 
 const Geography = () => {
